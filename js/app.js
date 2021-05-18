f9e887d9d1229ad687e0de2fe218fb9ffc7bc326
Quintin Kelly Dekker <quintinkellydekker888@gmail.com>
2021-05-18 17:49:07 +0200

    update

--- js/app.js
+++ js/app.js
@@ -6,32 +6,20 @@
 const navToggle = document.querySelector(".nav-toggle");
 const mobileNav = document.querySelector(".mobile-nav");
 
-navToggle.addEventListener('click', () => {
-    window.addEventListener('click', (e) => {
-        e.bubbles
-        e.target !== navToggle && mobileNav.classList.contains('show-mobile-nav') ?
-        mobileNav.classList.remove('show-mobile-nav') : null
+navToggle.addEventListener("click", () => {
+    window.addEventListener("click", (e) => {
+        e.target !== navToggle && mobileNav.classList.contains("show-mobile-nav") ?
+        mobileNav.classList.remove("show-mobile-nav") : null
     })
-    if (mobileNav.classList.contains('show-mobile-nav')) {
-        mobileNav.classList.remove('show-mobile-nav')
-    } else if (!mobileNav.classList.contains('show-mobile-nav')) {
-        mobileNav.classList.add('show-mobile-nav')
-    } else {
-        null
-    }
+    if (
+        mobileNav.classList.contains("show-mobile-nav")) {
+        mobileNav.classList.remove("show-mobile-nav")    
+        } else if (!mobileNav.classList.contains("show-mobile-nav")) {
+            mobileNav.classList.add("show-mobile-nav")
+        } else {
+            null
+        }
 })
-
-// window.onclick = (e) => {
-//     if (e.target.classList.contains("show-mobile-nav")) {
-//         e.target.classList.remove("show-mobile-nav");
-//     };
-// };
-
-// window.addEventListener("click", () => {
-//     if (window.classList.contains("show-mobile-nav")) {
-//         window.classList.remove("show-mobile-nav");
-//     };
-// });
 
 //********************************************** */
 //************ FLICKETY ************************** */
@@ -43,3 +31,18 @@ var flkty = new Flickity( elem, {
   contain: true
 });
 
+
+
+// navToggle.addEventListener('click', () => {
+//     window.addEventListener('click', (e) => {
+//         e.target !== navToggle && mobileNav.classList.contains('show-mobile-nav') ?
+//         mobileNav.classList.remove('show-mobile-nav') : null
+//     })
+//     if (mobileNav.classList.contains('show-mobile-nav')) {
+//         mobileNav.classList.remove('show-mobile-nav')
+//     } else if (!mobileNav.classList.contains('show-mobile-nav')) {
+//         mobileNav.classList.add('show-mobile-nav')
+//     } else {
+//         null
+//     }
+// })
