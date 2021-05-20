cde8295fc4af8bfae3df10837e0999a8d850b425
Quintin Kelly Dekker <quintinkellydekker888@gmail.com>
2021-05-20 18:04:31 +0200

    update

--- js/app.js
+++ js/app.js
@@ -6,19 +6,20 @@
 const navToggle = document.querySelector(".nav-toggle");
 const mobileNav = document.querySelector(".mobile-nav");
 
-// Listen for a click event on the navToggle
-// When navToggle is clicked. - if mobileNav is showing, hide it. if mobileNav is hidden, show it.
-// Listen for a click event on the navToggle & Body
-// Remove show-mobile-nav class when clicked on the navToggle or Body
+// Listen for a click on the navToggle
 
-navToggle.addEventListener("click",() => {
-    if (mobileNav.classList.contains("show-mobile-nav")) {
-        mobileNav.classList.remove("show-mobile-nav")
-    } else {
-        mobileNav.classList.add("show-mobile-nav")
-        document.body.addEventListener("click", () => {
-        })
-    }
+
+navToggle.addEventListener('click', () => {
+	if (!mobileNav.classList.contains('show-mobile-nav')) {
+		mobileNav.classList.add('show-mobile-nav')
+		document.body.addEventListener('click', e => {
+			if (!navToggle.contains(e.target)) {
+				mobileNav.classList.remove('show-mobile-nav')
+			}
+		})
+	} else {
+		mobileNav.classList.remove('show-mobile-nav')
+	}
 })
 
 //********************************************** */
