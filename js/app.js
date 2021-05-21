a1f12188afd038417cd483e88e22c325ee2f0870
Quintin Kelly Dekker <quintinkellydekker888@gmail.com>
2021-05-21 12:54:40 +0200

    update

--- js/app.js
+++ js/app.js
@@ -7,7 +7,10 @@ const navToggle = document.querySelector(".nav-toggle");
 const mobileNav = document.querySelector(".mobile-nav");
 
 // Listen for a click on the navToggle
-
+// check if mobile Nav contains Show Mobile nav class. If it doesn't add class
+// Listen for a click event on the body
+// if body contains show mobile nav class, remove it.
+// else, remove show mobile nav class.
 
 navToggle.addEventListener('click', () => {
 	if (!mobileNav.classList.contains('show-mobile-nav')) {
